39072320e9513529a4ee34483718ce59d827246c
Hulya Martli aka. Guerillagi <66822245+HulyaMartli@users.noreply.github.com>
2023-05-11 22:14:45 +0300

    control divided,userplay added,c added for cancel

--- js/script.js
+++ js/script.js
@@ -9,149 +9,143 @@ Rules of The Game:
 6. If you win one round you will gain 1 point! 💯
 That's it! Let's play! 🤹🏻`;
 
+const getInputMessage = "Enter your choice! \nRock, Paper or Scissors?";
 
-const inputNull = `Opps! You didn't wrote your choice. 😶
-You should write one of the following words:
+const inputEmpty = `Opps! YOU DIDN'T WRITE YOUR CHOICE. 😶
+You should write one of the following words in the box below before pressing OK button:
 "Rock", "paper" or "scissors"
 Now try again! Rock, Paper or Scissors?
-(Or press Cancel to exit 🏃🏻‍♀️)`;
+(If you want to exit please write "c"! 🏃🏻‍♀️)`;
 
 const inputMismatch = `Opps! Guess something went wrong. 🥲
-You should write one of the following words:
+Your input should include one of the following words only:
 "Rock", "paper" or "scissors"
 Now try again! Rock, Paper or Scissors?
-(Or press Cancel to exit 🏃🏻‍♀️)`
+(If you want to exit please write "c"! 🏃🏻‍♀️)`
 
 const userCanceled = `Did you pressed Cancel by mistake? 👀
 Let's continue!
-(If you want to exit press Cancel again!)`;
+(If you want to exit please write "c"! 🏃🏻‍♀️)`;
 
 const bye = `Guess you don't want to play? 🥹
 See you next time! 🙋🏻‍♀️
-(To restart the game please reload the page 🔄️)`
+(To restart the game please reload the page 🔄️)`;
 
-
-function control(userChoice) {
-    const choices = ["rock", "paper", "scissors"]
-    try {
-        if (!choices.includes(userChoice.toLowerCase().trim())) {
-            if (userChoice === "") {
-                userChoice = prompt(inputNull);
-                if (choices.includes(userChoice.toLowerCase().trim())) {
-                    return userChoice;
-                } else {
-                    userChoice = "";
-                    alert(bye);
-                    console.log("Bye :) Come again!")
-                }
-            } else if (userChoice === null) {
-                userChoice = prompt(userCanceled)
-                if (userChoice === null) {
-                    userChoice = "";
-                    alert(bye);
-                    console.log("Bye :) Come again!")
-                }
-            } else {
-                userChoice = prompt(inputMismatch);
-                try {
-                    if (choices.includes(userChoice.toLowerCase().trim())) {
-                        return userChoice;
-                    } else {
-                        userChoice = "";
-                        alert(bye);
-                        console.log("Bye :) Come again!")
-                    }
-                } catch (error) {
-                    userChoice = prompt(userCanceled)
-                    if (userChoice === null) {
-                        userChoice = "";
-                        alert(bye);
-                        console.log("Bye :) Come again!")
-                    }
-                }
-            }
-        }
-
-    } catch (error) {
-        userChoice = prompt(userCanceled)
-        if (userChoice === null) {
-            userChoice = "";
-            alert(bye);
-            console.log("Bye :) Come again!")
-        }
-    }
-
-    return userChoice;
-}
+let userScore = 0;
+let computerScore = 0;
+const choices = ["rock", "paper", "scissors"];
 
 function computerPlay() {
-    const choices = ["rock", "paper", "scissors"]
     let computerChoice = choices[Math.floor(Math.random() * choices.length)]
     return computerChoice;
 }
 
-function playRound(userChoice, computerChoice) {
-    if (userChoice !== "") {
-        if (userChoice.toLowerCase().trim() == computerChoice) {
-            return "It's a tie";
-        } else if ((userChoice.toLowerCase().trim() === "rock" && computerChoice === "scissors") || (userChoice.toLowerCase().trim() === "paper" && computerChoice === "rock") || (userChoice.toLowerCase().trim() === "scissors" && computerChoice === "paper")) {
-            return "You win the round! " + userChoice.toLowerCase().trim() + " beats " + computerChoice;
-        } else if ((userChoice.toLowerCase().trim() === "scissors" && computerChoice === "rock") || (userChoice.toLowerCase().trim() === "rock" && computerChoice === "paper") || (userChoice.toLowerCase().trim() === "paper" && computerChoice === "scissors")) {
-            return "You lose the round... " + computerChoice + " beats " + userChoice.toLowerCase().trim();
+function userPlay(userChoice) {
+    userChoice = prompt(getInputMessage);
+    do {
+        userChoice = inputControl(userChoice);
+        // controlling if user wants to exit:
+        if (userChoice === "c" || userChoice === "C") {
+            break;
         }
+    } while (!choices.includes(userChoice))
+    return userChoice;
+}
+
+function inputControl(userChoice) {
+    if (userChoice === null) {
+        userChoice = nullControl(userChoice)
+    } else if (userChoice === "") {
+        userChoice = emptyControl(userChoice);
+    } else if (!choices.includes(userChoice) &&
+        userChoice !== "c" && userChoice !== "C") {
+        userChoice = mismatchControl(userChoice);
+    }
+    return userChoice;
+}
+
+function nullControl(userChoice) {
+    while (userChoice === null) {
+        userChoice = prompt(userCanceled);
+    }
+    return userChoice;
+}
+
+function emptyControl(userChoice) {
+    while (userChoice === "") {
+        userChoice = prompt(inputEmpty)
+    }
+    return userChoice;
+}
+
+function mismatchControl(userChoice) {
+    while (!choices.includes(userChoice.toLowerCase().trim()) &&
+        userChoice !== "c" && userChoice !== "C"
+        && userChoice !== null && userChoice !== "") {
+        userChoice = prompt(inputMismatch);
+    }
+    userChoice=userChoice.toLowerCase().trim();
+    return userChoice;
+}
+
+function playRound(userChoice, computerChoice) {
+    if (userChoice == computerChoice) {
+        console.log("It's a tie");
+    } else if ((userChoice === "rock" && computerChoice === "scissors") ||
+        (userChoice === "paper" && computerChoice === "rock") ||
+        (userChoice === "scissors" && computerChoice === "paper")) {
+        console.log("You win the round! " + userChoice + " beats " + computerChoice);
+        userScore++;
+    } else if ((userChoice === "scissors" && computerChoice === "rock") ||
+        (userChoice === "rock" && computerChoice === "paper") ||
+        (userChoice === "paper" && computerChoice === "scissors")) {
+        console.log("You lose the round... " + computerChoice + " beats " + userChoice);
+        computerScore++;
+    }
+}
+
+function gameResult(userScore, computerScore) {
+    let finalResult = "Your Score: " + userScore +
+        " <---> " +
+        "Opponent Score: " + computerScore;
+    if (userScore === computerScore) {
+        console.log("GAME OVER\nIts' a tie!");
+        console.log(finalResult)
+    } else if (userScore > computerScore) {
+        console.log("GAME OVER\nYOU WIN!");
+        console.log(finalResult)
+        alert("GAME OVER\nYOU WIN!" + finalResult)
+    } else {
+        console.log("GAME OVER\nYou lose...");
+        console.log(finalResult)
+        alert("GAME OVER\nYou lose..." + finalResult)
     }
 }
 
 function game() {
-    let userScore = 0;
-    let computerScore = 0;
-    let roundResult;
-    let userChoice;
+    let userChoice = "";
+    let computerChoice = "";
+    alert(menu);
     for (let i = 0; i < 5; i++) {
-        const userInput = prompt("Enter your choice! \nRock, Paper or Scissors?");
-        userChoice = control(userInput);
-        if (userChoice === "") {
-            break;
-        }
-        let computerChoice = computerPlay();
-        console.log("==ROUND " + (i + 1) + "==");
-        roundResult = playRound(userChoice, computerChoice);
-        console.log("You: " + userChoice.toLowerCase().trim());
-        console.log("Opponent: " + computerChoice);
-        console.log(roundResult);
-        try {
-            if (roundResult.includes("win")) {
-                userScore++;
-            } else if (roundResult.includes("lose")) {
-                computerScore++;
-            }
-        } catch (error) {
+        userChoice = userPlay(userChoice);
+        // controlling if user wants to exit:
+        if (userChoice === "c" || userChoice === "C") {
             alert(bye);
             console.log("Bye :) Come again!")
+            break;
         }
-
+        if (choices.includes(userChoice)) {
+            computerChoice = computerPlay();
+            console.log("==ROUND " + (i + 1) + "==");
+            console.log("You: " + userChoice)
+            console.log("Opponent: " + computerChoice);
+            playRound(userChoice, computerChoice);
+        }
     }
-
-    if (userChoice !== "") {
-        let finalResult = "Your Score: " + userScore + " <---> " + "Opponent Score: " + computerScore;
-
-        if (userScore === computerScore) {
-            console.log("GAME OVER\nIts' a tie!");
-            console.log(finalResult)
-                ("GAME OVER\nIts' a tie!" + finalResult)
-        } else if (userScore > computerScore) {
-            console.log("GAME OVER\nYOU WIN!");
-            console.log(finalResult)
-            alert("GAME OVER\nYOU WIN!" + finalResult)
-        } else {
-            console.log("GAME OVER\nYou lose...");
-            console.log(finalResult)
-            alert("GAME OVER\nYou lose..." + finalResult)
-        }
+    if (choices.includes(userChoice)) {
+        gameResult(userScore, computerScore);
     }
 
 }
 
-alert(menu);
 game();
-
